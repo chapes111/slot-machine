1af3fabf09e0c7f66bacdb203d55d6f16ad62988
Shawn Tasker <shawntasker@Shawns-MacBook-Pro.local>
2023-05-16 17:09:19 -0400

    replace emojis and symbols with strings

--- odds.js
+++ odds.js
@@ -1,42 +1,42 @@
 export const symbolWeights = {
   // double jackpot
-  '💰': {
+  'DJ': {
     'reel1': 0,
     'reel2': 0,
     'reel3': 1
   },
   // blazing-7
-  '❤️‍🔥': {
+  'B7': {
     'reel1': 2,
     'reel2': 2,
     'reel3': 23,
   },
   // seven
-  '❤️‍🩹': {
+  'R7': {
     'reel1': 8,
     'reel2': 7,
     'reel3': 6
   },
   // triple-bar
-  '⫢': {
+  '3B': {
     'reel1': 6,
     'reel2': 6,
     'reel3': 3
   },
   // double-bar
-  '⊫': {
+  '2B': {
     'reel1': 11,
     'reel2': 11,
     'reel3': 6
   },
   // single-bar
-  '—': {
+  '1B': {
     'reel1': 17,
     'reel2': 17,
     'reel3': 9
   },
   // blank
-  '_': {
+  'BL': {
     'reel1': 28,
     'reel2': 29,
     'reel3': 24
